93bba74e89d423dcf7822d2f66c446fd2a84431e
ekinakyurek <akyurekekin@gmail.com>
2016-10-22 15:19:09 +0300

    changes

--- Linux/Drone/NetDrone2.py
+++ Linux/Drone/NetDrone2.py
@@ -19,70 +19,56 @@ clientPath = 'src/'
 #go to the client to get requests
 print "Scanning network area to connect Client to get requests"
 while(not foundClient):
-	for i in range (0,len(ssids)):
-		if (ssids[i] == clientSSID):
-			print("In the WiFi field of " + clientSSID)
-			foundClient = True
-			break
-	ssids = [cell.ssid for cell in Cell.all('wlan0')]
+    for i in range (0,len(ssids)):
+        if (ssids[i] == clientSSID):
+            print("In the WiFi field of " + clientSSID)
+                foundClient = True
+                    break
+        ssids = [cell.ssid for cell in Cell.all('wlan0')]
 
 print "Connecting to " + clientSSID
 wireless = Wireless()
 while(not connectedClient):
-	if wireless.connect(ssid=clientSSID, password=clientPass):
-		print "Connected to " + clientSSID
-		connectedClient = True
-	else:
-		print "Connection error"
+    if wireless.connect(ssid=clientSSID, password=clientPass):
+        print "Connected to " + clientSSID
+            connectedClient = True
+        else:
+            print "Connection error"
+
 
 
 if (connectedClient):
-	os.chdir(clientPath);
-	os.system("javac DroneAsServer.java"); #drone's class
-	os.system("java DroneAsServer");
+    os.chdir(clientPath);
+        #os.system("javac DroneAsServer.java"); #drone's class
+        os.system("java DroneAsServer");
 
 
 
 #go to the server
 connectedClient = False
 foundClient = False
+
 print "Scanning network area to connect Server (Web)"
 while(not foundServer):
-	for i in range (0,len(ssids)):
-		if (ssids[i] == serverSSID):
-			print("In the WiFi field of " + serverSSID)
-			foundServer = True
-			break
-	ssids = [cell.ssid for cell in Cell.all('wlan0')]
+    for i in range (0,len(ssids)):
+        if (ssids[i] == serverSSID):
+            print("In the WiFi field of " + serverSSID)
+                foundServer = True
+                    break
+        ssids = [cell.ssid for cell in Cell.all('wlan0')]
 
 print "Connecting to " + serverSSID
 wireless = Wireless()
 while(not connectedServer):
-	if wireless.connect(ssid=serverSSID, password=serverPass):
-		print "Connected to " + serverSSID
-		connectedServer = True
-	else:
-		print "Connection error"
+    if wireless.connect(ssid=serverSSID, password=serverPass):
+        print "Connected to " + serverSSID
+            connectedServer = True
+        else:
+            print "Connection error"
 
 if (connectedServer):
-	sitesList = []
-	f = open('sites.in','r')
-	site = f.readline()
-	while(site != ""):
-		sitesList.append(site)
-		site = f.readline()
-
-	print("Searching for links..")
-	for x in range(0,len(sitesList)):
-		length = len(sitesList[x])
-		link = sitesList[x][:length-1]
-		print("Visiting Link "+ link)
-		if (len(link) > 5):
-			comm = "wkhtmltopdf " + link + " Page"+ str(x+1)+".pdf"
-			os.system(comm)
-		os.chdir(clientPath);
-		os.system("javac DroneForServer.java"); #drone's class
-		os.system("java DroneForServer");
+    ## os.system("javac DroneForServer.java"); #drone's class
+    os.system("java DroneForServer");
 
 connectedServer = False
 foundServer = False
@@ -90,11 +76,11 @@ foundServer = False
 #go to the client to give back requests
 print "Scanning network area to connect Client (push back requests)"
 while(not foundClient):
-	for i in range (0,len(ssids)):
-		if (ssids[i] == clientSSID):
-			print("In the WiFi field of " + clientSSID)
-			foundClient = True
-			break
+    for i in range (0,len(ssids)):
+        if (ssids[i] == clientSSID):
+            print("In the WiFi field of " + clientSSID)
+                foundClient = True
+                    break
 	ssids = [cell.ssid for cell in Cell.all('wlan0')]
 
 print "Connecting to " + clientSSID
@@ -108,7 +94,6 @@ while(not connectedClient):
 
 
 if (connectedClient):
-	os.chdir(clientPath);
 	os.system("javac DroneAsServer.java"); #drone's class
 	os.system("java DroneAsServer");
 
